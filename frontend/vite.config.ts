8a4519c9b9a7add2e469cb21708771d74546fbe5
ahmed <ahmedslashabdulkarim@gmail.com>
2026-04-09 22:07:48 +0200

    update Vite-Proxy-Konfiguration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//Vite-Proxy-Konfiguration: ). Dies ist ein reines Entwicklungs-Feature,
+//         um CORS-Probleme zu umgehen und Anfragen an einen lokalen Backend-Server weiterzuleiten.
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+
+        /*
+        // Vorher
+          axios.get('http://localhost:8080/api/books')
+        // Nachher
+           axios.get('/api/books')
+        */
+
+      }
+    }
+  }
 })
+
